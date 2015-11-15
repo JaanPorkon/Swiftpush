6f9cc2cd90a992e0f7330f0c8c859d71f84c4d4f
Jaan Porkon <jaan@priorityprospect.com>
2015-11-15 10:21:57 -0800

    Fixed installation script and changed error message

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DIR="$(cd"$( dirname "${BASH_SOURCE[0]}")"&&pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod o+x swiftpush
-ln -s $DIR/swiftpush /usr/local/bin
+ln -s $DIR/swiftpush /usr/local/bin/swiftpush
