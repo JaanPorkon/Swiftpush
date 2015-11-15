60259a4cf01942a2d09b1dcc1c345d0d6cf69fb1
Jaan Porkon <jaan@priorityprospect.com>
2015-11-15 11:10:50 -0800

    Fixed configuration

--- cpanel-backup.sh
+++ cpanel-backup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-admin2
+container="PBN-Backups"
+date=`date +%Y-%m-%d`
+
+for user in /var/cpanel/users/*
+do
+  username=("${user/\/var\/cpanel\/users\//}")
+  /scripts/pkgacct $username
+  echo "Uploading: $username backup to $container"
+  swiftpush -f /home/cpmove-$username.tar.gz -p $date/cpmove-$username.tar.gz -c $container
+  echo "Upload done!"
+  echo "Removing internal backup file..."
+  rm -rf /home/cpmove-$username.tar.gz
+  echo "Internal backup file removed!"
+done
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 chmod o+x swiftpush
 ln -s $DIR/swiftpush /usr/local/bin/swiftpush
+chmod o+x cpanel-backup.sh
